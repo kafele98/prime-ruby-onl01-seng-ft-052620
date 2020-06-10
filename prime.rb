5b9caa593c1e906cdc7f7434d2f23650cbd14c99
Zoe Keller <keller.zoe98@gmail.com>
2020-06-10 04:13:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,12 @@
-def prime?(x)
-  if x == 1
-    return true 
-  elsif x < 1
-    return false
-  else
-  for y in 2..(x-1)
-    if (x % y) == 0
-      return false 
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
+                                # return true, then they are not divisible by the number and the number is prime
+      number % num_to_test == 0
     end
-  end 
-end
-  true
-end
+  else
+    false
+  end
+end 
